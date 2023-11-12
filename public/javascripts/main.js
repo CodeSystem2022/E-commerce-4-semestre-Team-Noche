42fd8821e3274ffcfa8ae0020597bc6a94f997d6
Franco J. Videla <91355557+odin1301@users.noreply.github.com>
2023-11-12 13:01:02 -0300

    Add files via upload

---
+++ public/javascripts/main.js
@@ -0,0 +1,12 @@
+// fade out para mensajes flash
+setTimeout(function () {
+  $("#flash-msg").fadeOut("slow");
+}, 3000);
+
+setTimeout(function () {
+  $("#success").fadeOut("slow");
+}, 3000);
+
+setTimeout(function () {
+  $("#error").fadeOut("slow");
+}, 3000);
